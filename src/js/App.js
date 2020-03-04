8134325cfc5911520e2235e418a3482cfee8a07a
Mduduzi <ndhlovu@pop-os.localdomain>
2020-03-04 16:24:04 +0000

    update app start

--- src/js/App.js
+++ src/js/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { render } from "react-dom";
 
 import AppContainer from "./containers/AppContainer";
 import BaseRouter from "./Routes";
 import store from "./store";
 
-const App = () => {
-  return (
-    (
+const APP_NAME = "react-template";
+
+export default class App extends Component {
+  start() {
+    this.launchApp({ userAuth: false });
+  }
+
+  launchApp(auth) {
+    const { userAuth } = auth;
+    render(
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={`${APP_NAME}?auth=${userAuth}`}>
           <AppContainer>
             <BaseRouter />
           </AppContainer>
         </BrowserRouter>
-      </Provider>
-    ),
-    document.getElementById("root")
-  );
-};
-export default App;
+      </Provider>,
+      document.getElementById("root")
+    );
+  }
+}
